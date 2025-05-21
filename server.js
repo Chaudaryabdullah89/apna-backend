2efc42efe9ae17fe82400aef76cffd0fb9c649c1
Chaudaryabdullah89 <chaudaryabdullah899@gmail.com>
2025-05-21 13:52:08 +0500

    12

--- server.js
+++ server.js
@@ -111,7 +111,8 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  exposedHeaders: ['Access-Control-Allow-Origin']
 };
 
 app.use(cors(corsOptions));
